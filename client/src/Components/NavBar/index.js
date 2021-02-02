f871bc0b91d9de363f377cffcf48ddd121c8f7c5
Fope <fopeayo@hotmail.com>
2021-02-02 09:21:16 +0000

    navbar

--- client/src/Components/NavBar/index.js
+++ client/src/Components/NavBar/index.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { SideBar } from './SideBar';
+import HomeIcon from './HomeIcon.png';
+import PieChartIcon from './PieChartIcon.png';
+import RecipeIcon from './RecipeIcon.png';
+import StatisticsIcon from './StatisticsIcon.png';
+// import './style.css'
 
-const NavBar = ({ isLoggedIn, logout }) => {
+    const NavBar = ({ isLoggedIn, logout }) => {
     return (
         <nav className='navbar'>
             { !isLoggedIn ?
                 <>
-                {/* <NavLink exact to='/'>Home</NavLink>
-                <NavLink exact to='/login'>Login</NavLink>
-                <NavLink exact to='/register'>Register</NavLink> */}
                 </>
                 :
                 <>
             <h2 id="idealMeTitle">idealMe</h2>
-                <NavLink to='/calories' className='nav' activeClassName='current'>Calories</NavLink>
-                <NavLink to='/recipe' className='nav' activeClassName='current'>Recipe</NavLink>
+            <div className='navOptions'>
+                <NavLink className= "image" to ='/Home' activeclassName="current">
+                 <img src={HomeIcon} className='icon' alt="Home" /> </NavLink>
+                <NavLink to='/Userdetails' className='image' activeClassName='current'>
+                <img src={StatisticsIcon} className='icon' alt="Stats" />  
+                 </NavLink>
+                <NavLink to='/Calories' className='image' activeClassName='current'>
+                <img src={PieChartIcon} className='icon' alt="Calorie-Counter" /></NavLink>
+                <NavLink to='/recipe' className='image' activeClassName='current'>
+                <img src={RecipeIcon} className='icon' alt="Recipes" /> 
+                </NavLink>
+                </div>
                 <button id="logoutbtn" onClick={logout}>Logout</button>
                 </>
             }
         </nav>
     );
 }
+
 
 export default NavBar;
