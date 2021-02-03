2789c42e66233576f5fd88018ee893e11c94a0fd
Raj Tandel <rajtandel@hotmail.com>
2021-02-03 17:07:39 +0000

    nav style

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import 'normalize.css';
+
+
 ReactDOM.render(
     <Router>
         <App />
